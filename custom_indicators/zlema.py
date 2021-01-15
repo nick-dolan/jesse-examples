511ed0c78b59ed9553d9cef7358b6cf0137b58a5
Nicolay Zlobin <nicolay.zlobin@gmail.com>
2021-01-15 15:13:09 +0300

    Refactor ZLEMA

--- custom_indicators/zlema.py
+++ custom_indicators/zlema.py
@@ -1,27 +1,31 @@
 from typing import Union
 import numpy as np
-import tulipy as ti
-from helpers import same_length
+import talib
+import pandas as pd
 
 
 def zlema(src, period=20, sequential=False) -> Union[float, np.ndarray]:
-    """
-    Zero-Lag Exponential Moving Average
-    The custom version of ZLEMA can receive a source array with nan inside.
+	"""
+	Zero-Lag Exponential Moving Average
+	This version of ZLEMA can receive source array with nan inside.
 
-    :param src: np.ndarray
-    :param period: int - default: 20
-    :param sequential: bool - default=False
+	:param src: np.ndarray
+	:param period: int - default: 20
+	:param sequential: bool - default=False
 
-    :return: float | np.ndarray
-    """
-    if not sequential and len(src) > 240:
-        src = src[-240:]
+	:return: float | np.ndarray
+	"""
+	if not sequential and len(src) > 240:
+		src = src[-240:]
 
-    source = src[np.logical_not(np.isnan(src))]  # Remove nan
-    res = ti.zlema(np.ascontiguousarray(source), period=period)
+	lag = (period - 1) / 2
+	src = pd.Series(src)
 
-    if sequential:
-        return same_length(src, np.concatenate((np.full((source.shape[0] - res.shape[0]), np.nan), res), axis=0))
-    else:
-        return res[-1]
+	ema = pd.Series(src + (src.diff(lag))).to_numpy()
+
+	res = talib.EMA(ema, timeperiod=period)
+
+	if sequential:
+		return res
+	else:
+		return res[-1]
